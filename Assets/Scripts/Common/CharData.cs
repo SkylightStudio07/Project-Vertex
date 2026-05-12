d6034348d929acbe1a3db0f6792081d4e51f934c
Bai Kyungmin <qorudalswl@naver.com>
2026-05-12 18:17:12 +0900

    Minor : CharData SO구조에 애니메이션 클립 필드 추가 생성

--- Assets/Scripts/Common/CharData.cs
+++ Assets/Scripts/Common/CharData.cs
@@ -8,8 +8,14 @@ public class CharData : ScriptableObject
     public string CharDescription;
 
     [Header("캐릭터 이미지(임시 용임. 나중엔 Addressable.Load로 불러오도록 설정)")]
-    public Sprite CharAnimationImage;
+    public Sprite CharImage;
     public Sprite CharIcon;
+
+    [Header("캐릭터 애니메이션")]
+    public AnimationClip IdleAnim;
+    public AnimationClip AttackAnim;
+    public AnimationClip HitAnim;
+    public AnimationClip DeathAnim;
 
     [Header("캐릭터 사운드")]
     public AudioClip CharSoundEffect;
