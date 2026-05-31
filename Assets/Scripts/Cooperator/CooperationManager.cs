b20e52025e51aa6d05b6e8b1d749c4939db00c0b
Bai Kyungmin <qorudalswl@naver.com>
2026-06-01 02:38:38 +0900

    Minor : 디버그용 코드 삭제

--- Assets/Scripts/Cooperator/CooperationManager.cs
+++ Assets/Scripts/Cooperator/CooperationManager.cs
@@ -18,9 +18,6 @@ public class CooperationManager : MonoBehaviour
 
     [SerializeField] private List<CoopCharData> coopCharList = new List<CoopCharData>();
     [SerializeField] private Dictionary<string, CoopCharState> coopCharDict = new Dictionary<string, CoopCharState>();
-
-    //Debug용 필드
-    [SerializeField] private CoopCharState debugCoopChar;
     
     private void Awake()
     {
@@ -52,20 +49,6 @@ public class CooperationManager : MonoBehaviour
                 isLevelUp = false,
                 rankEventDatasDict = rankEventDatasDict
             };
-
-            // Debug
-            debugCoopChar = new CoopCharState
-            {
-                charID = coopCharData.charID,
-                charData = coopCharData,
-                currentCoopLevel = 0,
-                currentCoopPoint = 0,
-                isLevelUp = false,
-                rankEventDatasDict = rankEventDatasDict
-            };
-            // Debug
-
-            Debug.Log($"{coopCharDict[coopCharData.charID].charID} 캐릭터 호감도 : {coopCharDict[coopCharData.charID].currentCoopPoint}");
         }
     }
 
@@ -103,20 +86,6 @@ public class CooperationManager : MonoBehaviour
     // 호감도 포인트 추가 메소드
     public void AddCoopPoint(string charID, int point)
     {
-        if (debugCoopChar.currentCoopLevel == 0) return;
-        //Debug
-        debugCoopChar.currentCoopPoint += point;
-        // 호감도 레벨업 체크
-        if (debugCoopChar.currentCoopLevel + 1 < debugCoopChar.charData.maxCoopLevel)
-        {
-            if (debugCoopChar.currentCoopPoint >= debugCoopChar.rankEventDatasDict[debugCoopChar.currentCoopLevel].requiredPoint)
-            {
-                debugCoopChar.isLevelUp = true;
-            }
-        }
-        return;
-        //Debug
-
         if (coopCharDict.TryGetValue(charID, out var charState))
         {
             if (charState.currentCoopLevel == 0) return;
@@ -137,25 +106,6 @@ public class CooperationManager : MonoBehaviour
 
     public void SettlePoint(string charID)
     {
-        //Debug
-        if (debugCoopChar.currentCoopLevel == 0) return;
-        // 호감도 레벨업 체크
-        if (debugCoopChar.currentCoopLevel + 1 <= debugCoopChar.charData.maxCoopLevel)
-        {
-            debugCoopChar.currentCoopPoint -= debugCoopChar.rankEventDatasDict[debugCoopChar.currentCoopLevel].requiredPoint;
-            if (debugCoopChar.currentCoopPoint >= debugCoopChar.rankEventDatasDict[debugCoopChar.currentCoopLevel].requiredPoint)
-            {
-                debugCoopChar.isLevelUp = true;
-            }
-            else
-            {
-                debugCoopChar.isLevelUp = false;
-            }
-            debugCoopChar.currentCoopLevel++;
-        }
-        return;
-        //Debug
-
         if (coopCharDict.TryGetValue(charID, out var charState))
         {
             if (charState.currentCoopLevel == 0) return;
@@ -177,23 +127,6 @@ public class CooperationManager : MonoBehaviour
             coopCharDict[charID] = charState;
         }
 
-    }
-
-    //호감도 포인트 증가하는지 테스트하는 메소드
-    public void AddCoopPoint_Debug()
-    {
-        AddCoopPoint("Cp_01", 2);
-    }
-
-    // 이벤트 완료 후 상태를 정산하는 메소드
-    public void SettlePoint_Debug()
-    {
-        SettlePoint("Cp_01");
-    }
-
-    public void UnlockCoopertor()
-    {
-        debugCoopChar.currentCoopLevel = 1;
     }
 }
 
