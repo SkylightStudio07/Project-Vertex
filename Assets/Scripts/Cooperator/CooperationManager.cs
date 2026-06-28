17347023af6ae99408f797b72db93350d23f9573
SkylightStudio07 <pys010725@koreatech.ac.kr>
2026-06-28 20:41:33 +0900

    [feature/enhancement] GameManager -> DeckManager 분리

--- Assets/Scripts/Cooperator/CooperationManager.cs
+++ Assets/Scripts/Cooperator/CooperationManager.cs
@@ -169,7 +169,7 @@ public class CooperationManager : MonoBehaviour
 
         if (coopCharData.joinRewardCard != null)
         {
-            GameManager.Instance.AddCardToPlayerDeck(coopCharData.joinRewardCard);
+            DeckManager.Instance.AddCardToPlayerDeck(coopCharData.joinRewardCard);
         }
 
         foreach (CardData card in coopCharData.battleRewardCardPool)
